97608771b2480ef3ca53c4a0b03084fb1f3a0c6e
uncleaar <64195573+uncleaar@users.noreply.github.com>
2023-07-22 00:48:48 +0600

    TT-13: board block (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,22 @@ import dynamicImport from "vite-plugin-dynamic-import";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- plugins: [react(), dynamicImport()],
- server: {
-	host: true,
-	port: 5173,
-	watch: {
-	 usePolling: true
+	plugins: [react(), dynamicImport()],
+	server: {
+		host: true,
+		port: 5173,
+		watch: {
+			usePolling: true
+		},
+		strictPort: true
 	},
-	strictPort: true
- },
- resolve: {
-	alias: {
-	 "@shared": path.resolve(__dirname, "./src/shared"),
-	 "@widgets": path.resolve(__dirname, "./src/widgets"),
-	 "@app": path.resolve(__dirname, "./src/app"),
-	 "@app/types": path.resolve(__dirname, "./src/app/types"),
-	 "@features": path.resolve(__dirname, "./src/features"),
-	 "@entities": path.resolve(__dirname, "./src/entities"),
+	resolve: {
+		alias: {
+			"@shared": path.resolve(__dirname, "./src/shared"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@app": path.resolve(__dirname, "./src/app"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@entities": path.resolve(__dirname, "./src/entities"),
+		}
 	}
- }
 });
