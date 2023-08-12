e59008c0a3c288eebc1411b106333d5ad09788f1
GetmevaDev <danilyudin723@gmail.com>
2023-08-12 17:29:04 +0600

    TT-46: create new board api, create project api

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,10 +6,9 @@ import { Sidebar } from "@widgets";
 import { AppRoute } from "./providers/router/AppRoute";
 
 export const App = () => (
-
 	<Box width="100%" height="100vh" bg="#f1f1f1">
 		<Container maxW="1470px">
-			 <Sidebar />
+			<Sidebar />
 			<Suspense fallback={<CircularProgress isIndeterminate color="green.300" />}>
 				<div className="content-page">
 					<AppRoute />
